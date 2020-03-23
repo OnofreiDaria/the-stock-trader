9aa944551436da2ca32e105ec75cb351d46a5c72
Daria Menscikova <menscikova.daria@gmail.com>
2020-03-23 12:42:55 +0100

    added stocks

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,55 @@
 <template>
-  <h1>Sample stock</h1>
+  <b-col sm="6" md="4" class="mt-5">
+    <b-card
+        title="Card Title"
+        header-tag="h4"
+        header-bg-variant="success"
+        text-variant="white"
+        style="max-width: 25rem;"
+    >
+      <template v-slot:header>{{ stock.name}}
+        <small>(Price: {{ stock.price }})</small>
+      </template>
+      <b-card-text>
+        <b-row>
+          <b-col cols="9">
+            <b-form-input placeholder="Quantity"
+                          type="number"
+                          v-model="quantity"></b-form-input>
+          </b-col>
+          <b-col cols="3">
+            <b-button variant="success"
+                      class="float-right"
+                      @click="buyStock"
+                      :disabled="quantity <= 0 || !Number.isInteger(+quantity)">
+              Buy</b-button>
+          </b-col>
+        </b-row>
+
+
+      </b-card-text>
+    </b-card>
+  </b-col>
 </template>
+
+<script>
+  export default  {
+    props: ['stock'],
+    data() {
+      return {
+        quantity: 0
+      }
+    },
+    methods: {
+      buyStock() {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity
+        };
+        console.log(order);
+        this.quantity = 0;
+      }
+    }
+  }
+</script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>Stocks</h1>
+  <b-container fluid>
+    <b-row>
+      <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+    </b-row>
+  </b-container>
+
 </template>
+<script>
+  import Stock from './Stock'
+
+  export default {
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'BMW', price: 120 },
+          { id: 2, name: 'Google', price: 200 },
+          { id: 3, name: 'Apple', price: 250 },
+          { id: 4, name: 'Twitter', price: 40 }
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
